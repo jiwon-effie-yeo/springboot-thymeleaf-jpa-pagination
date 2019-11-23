8b61885837bb9dc2537344965a57788c9bfe845c
DESKTOP-A828S0O\jiwon <C:\Users\\Documents\git_SourceTree>
2019-11-23 13:55:13 +0900

    Update Repository - page query

--- src/main/java/boot/jpa/pagination/domain/hero/HeroRepository.java
+++ src/main/java/boot/jpa/pagination/domain/hero/HeroRepository.java
@@ -1,7 +1,16 @@
 package boot.jpa.pagination.domain.hero;
 
+import boot.jpa.pagination.dto.HeroFindAllResponseDto;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.awt.print.Pageable;
 
 public interface HeroRepository extends JpaRepository<Hero, Long> {
 
+    @Query( "select h " +
+            "from Hero h " +
+            "order by h.id desc")
+    Page<HeroFindAllResponseDto> HeroFindAllResponse(Pageable pageable);
 }
